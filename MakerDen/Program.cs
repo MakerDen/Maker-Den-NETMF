51a7511c8a5a88e42d4fda903a934338bee56f76
gloveboxes <dglover.au@hotmail.com>
2014-12-07 19:22:33 +1100

    program.cs

--- MakerDen/Program.cs
+++ MakerDen/Program.cs
@@ -14,32 +14,6 @@ namespace MakerDen
         {
             // main code marker
 
-            PowerManagment.SetPeripheralState(Peripheral.Ethernet, Switch.Off);
-            PowerManagment.SetPeripheralState(Peripheral.SDCard, Switch.Off);
-            PowerManagment.SetPeripheralState(Peripheral.PowerLED, Switch.Off);
-
-            StartNeoPixel();
-
-            StartNetworkServices("dng", false);
-
-            using (SensorTemp temp = new SensorTemp(Pins.GPIO_PIN_D8, 10000, "temp01"))
-            using (SensorLight light = new SensorLight(AnalogChannels.ANALOG_PIN_A0, 1000, "light01"))
-            using (SensorMemory mem = new SensorMemory(5000, "mem01"))
-            using (SensorError error = new SensorError(5000, "error01"))
-            using (rgb = new RgbLed(Pins.GPIO_PIN_D3, Pins.GPIO_PIN_D5, Pins.GPIO_PIN_D6, "rgb01")) {
-
-                temp.OnBeforeMeasurement += OnBeforeMeasure;
-                temp.OnAfterMeasurement += OnMeasureCompleted;
-
-                light.OnBeforeMeasurement += OnBeforeMeasure;
-                light.OnAfterMeasurement += OnMeasureCompleted;
-
-                mem.OnBeforeMeasurement += OnBeforeMeasure;
-                mem.OnAfterMeasurement += OnMeasureCompleted;
-
-                error.OnAfterMeasurement += OnMeasureCompleted;
-
-                Thread.Sleep(Timeout.Infinite);
             }
         }
     }
