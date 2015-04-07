9c143291ffc60f742e593b4964a6d12b15897082
camiloborges <camilob@gmail.com>
2015-04-07 22:27:16 +1000

    created 'switch' sensor type. Relay and Valve inherit from it. Idea with inheritance is to ensure consistent naming for type

--- Glovebox.Netduino/Actuators/Valve.cs
+++ Glovebox.Netduino/Actuators/Valve.cs
@@ -7,15 +7,21 @@ using Glovebox.MicroFramework.Base;
 
 // www.dfrobot.com relay module V3.1
 namespace Glovebox.Netduino.Actuators {
-    public class Relay : Switch {
+    public class Valve : Switch {
+        public enum Actions {
+            On,
+            Off
+        }
+
+        private OutputPort switchPin;
 
         /// <summary>
         /// Create a relay control
         /// </summary>
         /// <param name="pin">From the SecretLabs.NETMF.Hardware.NetduinoPlus.Pins namespace</param>
         /// <param name="name">Unique identifying name for command and control</param>
-        public Relay(Cpu.Pin pin, string name)
-            : base(pin,name, "relay") {
+        public Valve(Cpu.Pin pin, string name)
+            : base(pin,name, "valve") {
         }
 
     }
