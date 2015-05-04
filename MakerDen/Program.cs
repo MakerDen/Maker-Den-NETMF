73adb4ac6d685cb7f20ba9be1042f28f329b4046
gloveboxes <dglover@microsoft.com>
2015-05-04 15:58:38 +1000

    MQTT NUGET Package Update

--- MakerDen/Program.cs
+++ MakerDen/Program.cs
@@ -11,21 +11,6 @@ namespace MakerDen {
 
             // main code maker
 
-            using (SensorTemp temp = new SensorTemp(Pins.GPIO_PIN_D8, 10000, "temp01"))
-            using (SensorLight light = new SensorLight(AnalogChannels.ANALOG_PIN_A0, 1000, "light01"))
-            using (rgb = new RgbLed(Pins.GPIO_PIN_D3, Pins.GPIO_PIN_D5, Pins.GPIO_PIN_D6, "rgb01")) {
-
-                while (true) {
-                    Debug.Print(light.ToString());
-                    Debug.Print(temp.ToString());
-
-                    rgb.On(RgbLed.Led.Red);
-                    Thread.Sleep(500);
-                    rgb.Off(RgbLed.Led.Red);
-                    Thread.Sleep(500);
-                }  // End of while loop
-            }
-
         }
     }
 }
